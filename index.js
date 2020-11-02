6a31cac739100807c71bf28ce54e904fe177ab4f
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-02 13:55:26 +0100

    function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Read instructions. And remove this line.
+
+
+
+function randomInt(Number) 
+ {
+   if (Number==69) {
+    return true 
+   }
+    else {
+        return false }
+    
+
+ }; 
+
+
+    console.log(randomInt(2))
+    console.log(randomInt(78))
+    console.log(randomInt(69))

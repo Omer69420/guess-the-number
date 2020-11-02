84d6e03dfa8bbfd6625b781cefe274b53f6e0c62
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-02 14:08:22 +0100

    tjabalaba

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ function randomInt(Number)
     console.log(randomInt(2))
     console.log(randomInt(78))
     console.log(randomInt(69))
+
+    function getUserGuess() {
+ const  stringValue = document.getElementById('user-input').value
+ return parseInt(stringValue, 69)
+    };
+    
+console.log(getUserGuess())

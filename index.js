4706b69bf663e0f005d9d83fdc824ee6d466aea3
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-09 12:43:57 +0100

    försökte

--- index.js
+++ index.js
@@ -2,23 +2,40 @@
 function randomInt(max){
     return Math.floor(Math.random() * Math.floor(max))
 }
-console.log(randomInt(100))
-console.log(randomInt(10))
-
+    const secretNumber = randomInt(100);
     
-    function getUserGuess() {
- const  stringValue = document.getElementById('user-input').value
- return parseInt(stringValue, 10)
-    };
+    console.log(secretNumber);
+    
+function getUserGuess() {
+    const  guess = document.getElementById('user-input').value;
+    return parseInt(guess, 10)
+}
 
-console.log(getUserGuess());
 
-document.addEventListener('keyup', function (event){
+document.addEventListener('keyup', keyPress);
    
-    if (event.key === 'Enter') {
-     const guess = getGuess()
-     
-       
- console.log(guess)
+function keyPress(enter) {
+    if (enter.key == 'Enter') {
+    console.log(getUserGuess + " " + secretNumber); 
+
+    if (getUserGuess() < secretNumber){
+        setMessage("Too low!");
     }
-})
+    else if (getUserGuess() > secretNumber){
+        setMessage("Too high!");
+    }
+    else {
+            setMessage("You are correct!");
+    }
+    clearInput()
+}
+
+function setMessage(msg){
+    console.log("setMessage")
+    let textfield = document.getElementById('message');
+    textfield.innerHTML = msg;
+}
+
+    function clearInput() {
+    document.getElementById('user-input').value = "";
+    }
